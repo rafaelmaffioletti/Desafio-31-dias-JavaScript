e0103522d9c62f457d6bde23108f4aa87664dd94
Rafael Maffioletti <maffioletti.rafa@gmail.com>
2025-01-08 23:22:19 -0300

    Adicionados as respostas dos exercicios 2, 3, 4 e 5

--- 07_funcoes.js
+++ 07_funcoes.js
@@ -9,9 +9,48 @@ console.log(retornoFuncao);
 
 // 2. Escreva uma função que receba um nome e retorne uma mensagem de boas-vindas.
 
+function saudacao(nome) {
+  return `Olá ${nome} seja bem vindo`;
+};
 
+console.log(saudacao("Rafael"));
 
 // 3. Crie uma função que receba dois números e retorne o maior deles.
+
+function maiorNumero(n1, n2) {
+  if (n1 > n2) {
+    return n1;
+  } else {
+    return n2;
+  }
+}
+
+let resultado = maiorNumero(10, 20);
+
+console.log(resultado)
+
 // 4. Implemente uma função que calcule a média de três números.
+
+function media(n1, n2, n3) {
+  return n1 + n2 + n3 / 3;
+
+};
+
+let n1 = 2;
+let n2 = 5;
+let n3 = 10;
+let resultadoMedia = media(n1, n2, n3);
+
+console.log(resultadoMedia.toFixed(2));
+
 // 5. Crie uma função que receba um array de números e retorne a soma deles.
+
+function somaArray(numeros) {
+  return numeros.reduce((soma, numero) => soma + numero, 0);
+}
+
+let numeros = [1, 2, 3, 4, 5];
+console.log(somaArray(numeros));
+
+
 // 6. Escreva uma função que receba uma string e retorne o número de caracteres dessa string.
